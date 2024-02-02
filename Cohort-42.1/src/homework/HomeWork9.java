5825fec7f8161d975aa59fdaa42d31066f30650d
Shilinski Rodion <Shilinski.rodion@outlook.com>
2024-02-02 21:48:38 +0100

    Lesson 9

--- Cohort-42.1/src/homework/HomeWork9.java
+++ Cohort-42.1/src/homework/HomeWork9.java
@@ -1,4 +1,13 @@
 package homework;
 
 public class HomeWork9 {
+    public static void main(String[] args) {
+
+    }
+
+    static void task1(int n) {
+        for (int i =1; i <= n; i++) {
+            
+        }
+    }
 }
--- Cohort-42.1/src/lesson9/Lesson9.java
+++ Cohort-42.1/src/lesson9/Lesson9.java
@@ -1,4 +1,37 @@
 package lesson9;
 
 public class Lesson9    {
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 5, 6, 1};
+        printArray(arr);
+
+        int[] arr2 = {-4, -1, -7, -9, -5};
+        printArray(arr2);
+
+        int aa = 2;
+        int bb = 2;
+        int r1 = add (aa, bb);
+        int r2 = add (-4,12,5,7);
+        System.out.println(r1 + ", " + r2);
+    }
+
+    static int add (int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+    static int add(int... a) {
+        int s = 0;
+        for (int i : a) {
+            s += i;
+        }
+        return s;
+    }
+
+    static void printArray (int[] a) {
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
