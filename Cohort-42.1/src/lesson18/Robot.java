51e0c7457df6190ff07065c1858e955ed7dc8d92
Shilinski Rodion <Shilinski.rodion@outlook.com>
2024-02-23 22:07:48 +0100

    Update

---
+++ Cohort-42.1/src/lesson18/Robot.java
@@ -0,0 +1,39 @@
+package lesson18;
+
+public class Robot {
+
+    public static final String COMPANY_NAME = "Boston Dynamics";
+
+    private int countCPU;
+
+    private int sum;
+    static int count;
+
+    public Robot(int countCPU) {
+        this.countCPU = countCPU;
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public void addAndSave(int a) {
+        sum += a;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        return "Robot{" +
+                "countCPU=" + countCPU +
+                '}';
+    }
+}
