9690c610b5d78bf918bf58ed4cfe80383ed83de7
Shilinski Rodion <Shilinski.rodion@outlook.com>
2024-04-26 17:50:29 +0200

    Update
    Special thanks to Anrey Hein

--- Cohort-42.1/src/lesson42/homework/Homework.java
+++ Cohort-42.1/src/lesson42/homework/Homework.java
@@ -1,6 +1,7 @@
 package lesson42.homework;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Homework {
     public static void main(String[] args) {
@@ -11,12 +12,17 @@ public class Homework {
             numbers.add(i);
         }
 
-        List<Integer> evenNumbers = new ArrayList<>();
-        for (Integer even : numbers) {
-            if (even % 2 == 0) {
-                evenNumbers.add(even);
-            }
-        }
+        List<Integer> evenNumbers = numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .collect(Collectors.toList());
+
+
+//        List<Integer> evenNumbers = new ArrayList<>();
+//        for (Integer even : numbers) {
+//            if (even % 2 == 0) {
+//                evenNumbers.add(even);
+//            }
+//        }
 
         System.out.println(numbers);
         System.out.println(evenNumbers);
@@ -30,6 +36,15 @@ public class Homework {
 
         System.out.println(words);
 
+        List<String> strList = List.of("hEllO", "JaVa", "LAMBDA");
+
+        List<String> updWords = strList.stream()
+                .map(word ->word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase())
+                .collect(Collectors.toList());
+
+        System.out.println("Unconverted list: " + strList);
+        System.out.println("Converted list: " + updWords);
+        
         //task3
 
 
